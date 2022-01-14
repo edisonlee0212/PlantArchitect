bbb9117fe81fb90677445bee57071e0f0716d28d
Bosheng Li <li2343@purdue.edu>
2022-01-14 01:33:35 -0500

    CP.

--- src/Scripts/GANTreePipelineDriver.cpp
+++ src/Scripts/GANTreePipelineDriver.cpp
@@ -17,13 +17,7 @@ void GANTreePipelineDriver::OnInspect() {
             if (std::filesystem::exists(m_folderPath) && std::filesystem::is_directory(m_folderPath)) {
                 for (const auto &entry: std::filesystem::directory_iterator(m_folderPath)) {
                     if (!std::filesystem::is_directory(entry.path())) {
-                        if (entry.path().filename().string() == "Tulip.gtparams") {
-                            m_parameterFilePaths.push_back(entry.path());
-                        }
-                        if (entry.path().filename().string() == "Birch.gtparams") {
-                            m_parameterFilePaths.push_back(entry.path());
-                        }
-                        if (entry.path().filename().string() == "Corkscrew.gtparams") {
+                        if (entry.path().filename().string() == "Elm.gtparams") {
                             m_parameterFilePaths.push_back(entry.path());
                         }
                     }
