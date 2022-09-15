dcafda12285dca42193c5dad44486c8953ff2a49
Bosheng Li <li2343@purdue.edu>
2022-09-15 02:21:03 -0400

    Checkpoint.

--- include/PlantArchitect/StrandModel/Strand.hpp
+++ include/PlantArchitect/StrandModel/Strand.hpp
@@ -11,6 +11,15 @@ namespace PlantArchitect {
         std::weak_ptr<StrandKnot> m_prev;
         std::weak_ptr<StrandKnot> m_next;
     public:
+        std::weak_ptr<StrandKnot> m_upLeft;
+        std::weak_ptr<StrandKnot> m_upRight;
+        std::weak_ptr<StrandKnot> m_right;
+        std::weak_ptr<StrandKnot> m_downRight;
+        std::weak_ptr<StrandKnot> m_downLeft;
+        std::weak_ptr<StrandKnot> m_left;
+
+        int m_distanceToBoundary = 0;
+        glm::ivec2 m_coordinate = glm::ivec2(0);
         glm::vec3 m_position = glm::vec3(0.0f);
     };
 
@@ -31,12 +40,13 @@ namespace PlantArchitect {
         float m_pointDistance = 0.01f;
         std::vector<glm::vec2> m_regionBoundary;
         glm::vec2 m_boundaryRadius = glm::vec2(0.0f);
-        std::vector<glm::vec2> m_points;
+        int m_maxDistanceToEnd = 0;
         void DisplayIntersection(const std::string& title, bool editable);
     public:
         void SetPointDistance(float value);
 
         void FillPoints();
+        void CalculateConnectivity();
         void OnCreate() override;
         void OnInspect() override;
         void Construct(const std::vector<glm::vec2>& points);
--- src/PlantArchitect/Strand.cpp
+++ src/PlantArchitect/Strand.cpp
@@ -4,7 +4,18 @@
 
 #include "Strand.hpp"
 #include "DataComponents.hpp"
+
 using namespace PlantArchitect;
+
+glm::vec2 GetPosition(const glm::ivec2 &coordinate, float pointDistance) {
+    return glm::vec2(coordinate.x * pointDistance + coordinate.y * pointDistance / 2.0f,
+                     coordinate.y * pointDistance * glm::cos(glm::radians(30.0f)));
+}
+
+glm::ivec2 GetCoordinate(const glm::vec2 &position, float pointDistance) {
+    int y = glm::round(position.y / pointDistance / glm::cos(glm::radians(30.0f)));
+    return glm::ivec2(glm::round((position.x - y * pointDistance / 2.0f) / pointDistance), y);
+}
 
 void StrandPlant::GenerateStrands(float pointDistance) {
     auto scene = GetScene();
@@ -18,7 +29,7 @@ void StrandPlant::OnInspect() {
     if (ImGui::Button("Generate Strands")) GenerateStrands();
     static bool displayRootIntersection = true;
     ImGui::Checkbox("Display root intersection", &displayRootIntersection);
-    if(displayRootIntersection){
+    if (displayRootIntersection) {
         auto scene = GetScene();
         auto rootEntity = GetRoot();
         auto rootIntersection = scene->GetOrSetPrivateComponent<StrandsIntersection>(rootEntity).lock();
@@ -35,16 +46,16 @@ Entity StrandPlant::GetRoot() {
     auto children = scene->GetChildren(self);
     bool found = false;
     Entity retVal;
-    for(const auto& child : children){
-        if(scene->HasPrivateComponent<StrandsIntersection>(child)){
-            if(found) scene->DeleteEntity(child);
-            else{
+    for (const auto &child: children) {
+        if (scene->HasPrivateComponent<StrandsIntersection>(child)) {
+            if (found) scene->DeleteEntity(child);
+            else {
                 found = true;
                 retVal = child;
             }
         }
     }
-    if(!found) {
+    if (!found) {
         retVal = scene->CreateEntity("Root");
         scene->AddDataComponent<StrandIntersectionInfo>(retVal, StrandIntersectionInfo());
         auto rootIntersection = scene->GetOrSetPrivateComponent<StrandsIntersection>(retVal).lock();
@@ -55,7 +66,6 @@ Entity StrandPlant::GetRoot() {
 }
 
 void StrandsIntersection::Construct(const std::vector<glm::vec2> &points) {
-    m_points.clear();
     m_regionBoundary = points;
     //1. Calculate min/max bound
     auto max = glm::vec2(FLT_MIN);
@@ -109,7 +119,7 @@ bool StrandsIntersection::IsInRegion(const glm::vec2 &point) const {
     const auto point2 = glm::vec2(1.0f);
     int windingNumber = 0;
     const auto size = m_regionBoundary.size();
-    if(size < 3) return false;
+    if (size < 3) return false;
     for (int i = 0; i < size - 1; i++) {
         if (RayLineIntersect(point, point2, m_regionBoundary[i], m_regionBoundary[i + 1])) {
             windingNumber++;
@@ -121,15 +131,16 @@ bool StrandsIntersection::IsInRegion(const glm::vec2 &point) const {
     }
     return false;
 }
-void StrandsIntersection::DisplayIntersection(const std::string& title, bool editable) {
+
+void StrandsIntersection::DisplayIntersection(const std::string &title, bool editable) {
     if (ImGui::Begin(title.c_str())) {
         static auto scrolling = glm::vec2(0.0f);
         static float zoomFactor = 1000.0f;
         if (ImGui::Button("Recenter")) {
             scrolling = glm::vec2(0.0f);
         }
-        ImGui::DragFloat("Zoom", &zoomFactor, 1.0f, 100.0f, 2500.0f);
-        zoomFactor = glm::clamp(zoomFactor, 100.0f, 2500.0f);
+        ImGui::DragFloat("Zoom", &zoomFactor, zoomFactor / 100.0f, 100.0f, 5000.0f);
+        zoomFactor = glm::clamp(zoomFactor, 100.0f, 5000.0f);
         ImGuiIO &io = ImGui::GetIO();
         ImDrawList *draw_list = ImGui::GetWindowDrawList();
 
@@ -203,12 +214,21 @@ void StrandsIntersection::DisplayIntersection(const std::string& title, bool edi
                                       origin.y + m_regionBoundary[0].y * zoomFactor),
                                IM_COL32(255, 255, 0, 255), 2.0f);
 
-            for (const auto &point: m_points) {
-                auto position = ImVec2(origin.x + point.x * zoomFactor,
-                                       origin.y + point.y * zoomFactor);
-                draw_list->AddCircleFilled(position, glm::clamp(m_pointDistance * 0.4f * zoomFactor, 1.0f, 100.0f),
-                                           IM_COL32(255, 255, 255, 255));
+            for (const auto &knot: m_strandKnots) {
+                auto pointPosition = GetPosition(knot->m_coordinate, m_pointDistance);
+                auto canvasPosition = ImVec2(origin.x + pointPosition.x * zoomFactor,
+                                             origin.y + pointPosition.y * zoomFactor);
+                draw_list->AddCircleFilled(canvasPosition,
+                                           glm::clamp(m_pointDistance * 0.4f * zoomFactor, 1.0f, 100.0f),
+                                           IM_COL32(255.0f * knot->m_distanceToBoundary / m_maxDistanceToEnd, 255.0f * knot->m_distanceToBoundary / m_maxDistanceToEnd, 255.0f * knot->m_distanceToBoundary / m_maxDistanceToEnd, 255));
+                if(zoomFactor > 2000) {
+                    auto textCanvasPosition = ImVec2(origin.x + pointPosition.x * zoomFactor - 0.003f * zoomFactor,
+                                                 origin.y + pointPosition.y * zoomFactor - 0.003f * zoomFactor);
+                    auto text = std::to_string(knot->m_distanceToBoundary);
+                    draw_list->AddText(0, 0.005f * zoomFactor, textCanvasPosition, IM_COL32(255, 0, 0, 255), text.c_str());
+                }
             }
+
         } else {
             const auto size = points.size();
             for (int i = 0; i < size - 1; i++) {
@@ -231,29 +251,170 @@ void StrandsIntersection::DisplayIntersection(const std::string& title, bool edi
     }
     ImGui::End();
 }
+
 void StrandsIntersection::OnInspect() {
     static bool displayRegion = true;
     ImGui::Checkbox("Display Intersection", &displayRegion);
-    if(displayRegion) {
+    if (displayRegion) {
         DisplayIntersection("Intersection", false);
     }
 }
+
 void StrandsIntersection::SetPointDistance(float value) {
     m_pointDistance = value;
     FillPoints();
 }
+
 void StrandsIntersection::FillPoints() {
-    m_points.clear();
+    m_strandKnots.clear();
     int yRange = glm::ceil(m_boundaryRadius.y / (m_pointDistance * glm::cos(glm::radians(30.0f))));
     int xRange = glm::ceil(m_boundaryRadius.x / m_pointDistance);
     for (int i = -xRange; i <= xRange; i++) {
         for (int j = -yRange; j <= yRange; j++) {
-            glm::vec2 point;
-            point.x = i * m_pointDistance + (j % 2 == 0 ? 0 : 0.5f * m_pointDistance);
-            point.y = j * m_pointDistance * glm::cos(glm::radians(30.0f));
-            if (IsInRegion(point)) {
-                m_points.push_back(point);
+            glm::ivec2 coordinate;
+            coordinate.y = j;
+            coordinate.x = i - j / 2;
+            if (IsInRegion(GetPosition(coordinate, m_pointDistance))) {
+                m_strandKnots.push_back(std::make_shared<StrandKnot>());
+                m_strandKnots.back()->m_coordinate = coordinate;
             }
         }
+    }
+    CalculateConnectivity();
+}
+
+void StrandsIntersection::CalculateConnectivity() {
+    m_maxDistanceToEnd = 0;
+    std::map<std::pair<int, int>, std::shared_ptr<StrandKnot>> knotsMap;
+    for (const auto &knot: m_strandKnots) {
+        knot->m_distanceToBoundary = 99999;
+        knotsMap[{knot->m_coordinate.x, knot->m_coordinate.y}] = knot;
+    }
+
+    std::queue<std::shared_ptr<StrandKnot>> knotDistanceCalculationQueue;
+    std::set<std::shared_ptr<StrandKnot>> visitedKnots;
+    for (auto &knot: m_strandKnots) {
+        int x = knot->m_coordinate.x;
+        int y = knot->m_coordinate.y;
+        knot->m_upLeft.reset();
+        knot->m_upRight.reset();
+        knot->m_right.reset();
+        knot->m_downRight.reset();
+        knot->m_downLeft.reset();
+        knot->m_left.reset();
+        auto search = knotsMap.find({x - 1, y + 1});
+        if (search != knotsMap.end()) {
+            knot->m_upLeft = search->second;
+        } else {
+            knot->m_distanceToBoundary = 0;
+            knotDistanceCalculationQueue.push(knot);
+            visitedKnots.emplace(knot);
+        }
+        search = knotsMap.find({x, y + 1});
+        if (search != knotsMap.end()) {
+            knot->m_upRight = search->second;
+        } else {
+            knot->m_distanceToBoundary = 0;
+            knotDistanceCalculationQueue.push(knot);
+            visitedKnots.emplace(knot);
+        }
+        search = knotsMap.find({x + 1, y});
+        if (search != knotsMap.end()) {
+            knot->m_right = search->second;
+        } else {
+            knot->m_distanceToBoundary = 0;
+            knotDistanceCalculationQueue.push(knot);
+            visitedKnots.emplace(knot);
+        }
+        search = knotsMap.find({x + 1, y - 1});
+        if (search != knotsMap.end()) {
+            knot->m_downRight = search->second;
+        } else {
+            knot->m_distanceToBoundary = 0;
+            knotDistanceCalculationQueue.push(knot);
+            visitedKnots.emplace(knot);
+        }
+        search = knotsMap.find({x, y - 1});
+        if (search != knotsMap.end()) {
+            knot->m_downLeft = search->second;
+        } else {
+            knot->m_distanceToBoundary = 0;
+            knotDistanceCalculationQueue.push(knot);
+            visitedKnots.emplace(knot);
+        }
+        search = knotsMap.find({x - 1, y});
+        if (search != knotsMap.end()) {
+            knot->m_left = search->second;
+        } else {
+            knot->m_distanceToBoundary = 0;
+            knotDistanceCalculationQueue.push(knot);
+            visitedKnots.emplace(knot);
+        }
+    }
+
+    while (!knotDistanceCalculationQueue.empty()) {
+        auto currentKnot = knotDistanceCalculationQueue.front();
+        knotDistanceCalculationQueue.pop();
+        if (!currentKnot->m_upLeft.expired()) {
+            auto visitingKnot = currentKnot->m_upLeft.lock();
+            if (currentKnot->m_distanceToBoundary + 1 < visitingKnot->m_distanceToBoundary) {
+                visitingKnot->m_distanceToBoundary = currentKnot->m_distanceToBoundary + 1;
+            }
+            if (visitedKnots.find(visitingKnot) == visitedKnots.end()) {
+                visitedKnots.emplace(visitingKnot);
+                knotDistanceCalculationQueue.push(visitingKnot);
+            }
+        }
+        if (!currentKnot->m_upRight.expired()) {
+            auto visitingKnot = currentKnot->m_upRight.lock();
+            if (currentKnot->m_distanceToBoundary + 1 < visitingKnot->m_distanceToBoundary) {
+                visitingKnot->m_distanceToBoundary = currentKnot->m_distanceToBoundary + 1;
+            }
+            if (visitedKnots.find(visitingKnot) == visitedKnots.end()) {
+                visitedKnots.emplace(visitingKnot);
+                knotDistanceCalculationQueue.push(visitingKnot);
+            }
+        }
+        if (!currentKnot->m_right.expired()) {
+            auto visitingKnot = currentKnot->m_right.lock();
+            if (currentKnot->m_distanceToBoundary + 1 < visitingKnot->m_distanceToBoundary) {
+                visitingKnot->m_distanceToBoundary = currentKnot->m_distanceToBoundary + 1;
+            }
+            if (visitedKnots.find(visitingKnot) == visitedKnots.end()) {
+                visitedKnots.emplace(visitingKnot);
+                knotDistanceCalculationQueue.push(visitingKnot);
+            }
+        }
+        if (!currentKnot->m_downRight.expired()) {
+            auto visitingKnot = currentKnot->m_downRight.lock();
+            if (currentKnot->m_distanceToBoundary + 1 < visitingKnot->m_distanceToBoundary) {
+                visitingKnot->m_distanceToBoundary = currentKnot->m_distanceToBoundary + 1;
+            }
+            if (visitedKnots.find(visitingKnot) == visitedKnots.end()) {
+                visitedKnots.emplace(visitingKnot);
+                knotDistanceCalculationQueue.push(visitingKnot);
+            }
+        }
+        if (!currentKnot->m_downLeft.expired()) {
+            auto visitingKnot = currentKnot->m_downLeft.lock();
+            if (currentKnot->m_distanceToBoundary + 1 < visitingKnot->m_distanceToBoundary) {
+                visitingKnot->m_distanceToBoundary = currentKnot->m_distanceToBoundary + 1;
+            }
+            if (visitedKnots.find(visitingKnot) == visitedKnots.end()) {
+                visitedKnots.emplace(visitingKnot);
+                knotDistanceCalculationQueue.push(visitingKnot);
+            }
+        }
+        if (!currentKnot->m_left.expired()) {
+            auto visitingKnot = currentKnot->m_left.lock();
+            if (currentKnot->m_distanceToBoundary + 1 < visitingKnot->m_distanceToBoundary) {
+                visitingKnot->m_distanceToBoundary = currentKnot->m_distanceToBoundary + 1;
+            }
+            if (visitedKnots.find(visitingKnot) == visitedKnots.end()) {
+                visitedKnots.emplace(visitingKnot);
+                knotDistanceCalculationQueue.push(visitingKnot);
+            }
+        }
+        if(currentKnot->m_distanceToBoundary > m_maxDistanceToEnd) m_maxDistanceToEnd = currentKnot->m_distanceToBoundary;
     }
 }
